a57b1bf1cba32ddc7419acf3f7448f42ed63b207
jennylynnlong <88349512+jennylynnlong@users.noreply.github.com>
2021-09-28 15:16:55 -0400

    Update main.py
    Forgot to save

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,36 @@
 import os
 import csv
 
+# path to collect data from Resources folder
 budget_csv = os.path.join("..", "Resources", "budget_data.csv")
 
-with open(budget_csv) as csvFile:
+# variables
+totalMonths = 0 # initialize total months to 0
+
+# Define the function
+def financial_analysis(budget_csv):
+
+    # month is at index 0, profit/losses at index 1
+    month = budget_csv[0]
+    profit_loss = int(budget_csv[1])
+
+    # find total months
+
+
+
+    # print analysis
+    print("Financial Analysis")
+    print("------------------------------")
+    print(f"")
+
+# Read in the csv file
+with open(budget_csv, 'r') as csvFile:
+    
+    # Split the data on commas
     csvreader = csv.reader(csvFile, delimiter=",")
 
-    csv_header = next(csvFile)
+    # read header row
+    csv_header = next(csvreader)
 
-    # initialize total months
-    totalMonths = 0
+   
 
