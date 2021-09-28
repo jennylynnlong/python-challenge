4214a241a1c65a09ae6a512628e4846d57b77bf8
jennylynnlong <88349512+jennylynnlong@users.noreply.github.com>
2021-09-28 15:15:28 -0400

    Basic setup
    started defining financial_analysis function, csv read is unfinished

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,13 @@
-main script
+import os
+import csv
+
+budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+with open(budget_csv) as csvFile:
+    csvreader = csv.reader(csvFile, delimiter=",")
+
+    csv_header = next(csvFile)
+
+    # initialize total months
+    totalMonths = 0
+
