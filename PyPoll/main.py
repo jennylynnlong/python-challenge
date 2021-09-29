ea2f478365cff631c67614637a5cfc135f496123
jennylynnlong <88349512+jennylynnlong@users.noreply.github.com>
2021-09-29 10:33:32 -0400

    Final election results
    Complete python code for election results and created text file with results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,6 +10,9 @@ outputfile = os.path.join("electionResults.txt")
 # variables
 totalVotes = 0  # initialize total votes to 0
 candidates = [] # initialize the list of candidates
+candidateVotes = {}  # initialize dictionary of candidate votes
+winningCount = 0    # initialize winning count to 0
+winningCandidate = ""   # to hold winning candidate
 
 # Read in the csv file
 with open(election_csv, 'r') as csvFile:
@@ -19,9 +22,7 @@ with open(election_csv, 'r') as csvFile:
 
     # read header row
     csv_header = next(csvreader)
-    # move to the first row
-    firstRow = next(csvreader)
-
+    
     # increment the count of the total votes
     totalVotes += 1
 
@@ -29,18 +30,43 @@ with open(election_csv, 'r') as csvFile:
         # increment the count of the total months
         totalVotes += 1
 
+        # check if candidates (in index[2]) is in candidates list
+        if row[2] not in candidates:
+            # add candidate to list if not there already
+            candidates.append(row[2])
+
+            # add the candidate to the dictionary as well; start the vote counts at 1
+            candidateVotes[row[2]] = 1
+        
+        else:
+            # if candidate is in list already, add vote to candidateVotes
+            candidateVotes[row[2]] += 1
+
+vote_output = ""
+
+for candidates in candidateVotes:
+        #get the vote count and percentage of the votes
+        votes = candidateVotes.get(candidates)
+        percentageVotes = (float(votes) / float(totalVotes)) *100
+
+        vote_output += f"{candidates}: {percentageVotes:.3f}% ({votes}) \n"
+
+        # compare votes to winning count
+        if votes > winningCount:
+            # udpate the winning count
+            winningCount = votes
+            # update winning candidate
+            winningCandidate = candidates
+
 # generating the output
 output = (
     f"\nElection Results \n"
     f"------------------------- \n"
     f"Total Votes: {totalVotes} \n"
     f"------------------------- \n"
-    f"Khan: \n"
-    f"Correy: \n"
-    f"Li: \n"
-    f"O'Tooley: \n"
+    f"{vote_output}"
     f"------------------------- \n"
-    f"Winner: \n"
+    f"Winner: {winningCandidate}\n"
     f"------------------------- \n"
 )
 
