b29c5ec0a3d15b14faf536f6b6cd1e164df32af7
jennylynnlong <88349512+jennylynnlong@users.noreply.github.com>
2021-09-29 08:20:29 -0400

    Created output and text file
    Created totalMonths, and output for printing, and a text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,26 +2,13 @@ import os
 import csv
 
 # path to collect data from Resources folder
-budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+budget_csv = os.path.join("Resources", "budget_data.csv")
+
+# file to hold the output of the budget analysis
+outputfile = os.path.join("budgetAnalysis.txt")
 
 # variables
 totalMonths = 0 # initialize total months to 0
-
-# Define the function
-def financial_analysis(budget_csv):
-
-    # month is at index 0, profit/losses at index 1
-    month = budget_csv[0]
-    profit_loss = int(budget_csv[1])
-
-    # find total months
-
-
-
-    # print analysis
-    print("Financial Analysis")
-    print("------------------------------")
-    print(f"")
 
 # Read in the csv file
 with open(budget_csv, 'r') as csvFile:
@@ -32,5 +19,20 @@ with open(budget_csv, 'r') as csvFile:
     # read header row
     csv_header = next(csvreader)
 
-   
+    for row in csvreader:
+        # increment the count of the total months
+        totalMonths += 1
 
+# start generating the output
+output = (
+    f"\nFinancial Analysis \n"
+    f"--------------------------------- \n"
+    f"Total Months: {totalMonths}"
+)
+
+# print the output to the terminal
+print(output)
+
+# export the output variable to the text file
+with open(outputfile, "w") as textfile:
+    textfile.write(output)
